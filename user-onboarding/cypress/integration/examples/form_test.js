effafcbb3793d13c7d64572af120166d1e419dc2
Rachele Edwards <dot.com1472@gmail.com>
2020-03-26 13:27:48 -0600

    first had to delete and reinstall npm folders to allow server to pop up. Finished test requirements. On to stretch XD

---
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -0,0 +1,21 @@
+describe("Testing our inputs and submit our form", function(){
+    beforeEach(function(){
+        cy.visit("http://localhost:3001")
+    })
+    it("Will fill our form inputs and submit form", function(){
+        cy.get('input[name="name"]')
+          .type("Rachele Edwards")
+          .should("have.value", "Rachele Edwards")
+        cy.get('input[name="email"]')
+          .type("thundrchikn@gmail.com")
+          .should("have.value", "thundrchikn@gmail.com")
+        cy.get('input[name="password"]')
+          .type("HelloWorld")
+          .should("have.value", "HelloWorld")
+        cy.get('input[type="checkbox"]')
+          .check()
+          .should("be.checked")
+        cy.get("button")
+          .click()
+    })
+})
